d7f145d544ee7f373d630b5bb19def3f6168901b
haswitha18 <70908134+haswitha18@users.noreply.github.com>
2020-09-13 11:40:06 +0530

    Update hash_chatbot.py

--- hash_chatbot.py
+++ hash_chatbot.py
@@ -16,7 +16,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-from telegram.ext import Updater, CommandHandler,MessageHandler, Filters 
+from telegram.ext import Updater, CommandHandler,MessageHandler, Filters  
 import requests #Getting the data from the cloud
     
 def ledoff(bot,update):
